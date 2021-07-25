9b31218b9d2906eea66211431d84bb7de6858f4e
Eduardo Shiota Yasuda <contato@eshiota.com>
2021-07-25 23:09:04 +0200

    Fix type

--- src/api.ts
+++ src/api.ts
@@ -1,12 +1,12 @@
 const apiKey = "47c1cb2f54db836153c5958bbe5b9678181f1949";
 
-export async function getGames<GetGamesResponse>({
+export async function getGames({
   page,
   limit,
 }: {
   page: number;
   limit: number;
-}) {
+}): Promise<GetGamesResponse> {
   const res = await fetch(
     `https://www.giantbomb.com/api/games/?api_key=${apiKey}&format=json&sort=name:asc&limit=${limit}&offset=${
       page * limit
