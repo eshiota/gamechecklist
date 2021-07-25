ae01fbd6a28040dbb8202e83447ffc3bf1c7dfc8
Eduardo Shiota Yasuda <contato@eshiota.com>
2021-07-25 19:48:59 +0200

    Add thumbnail

--- src/components/GamesList.svelte
+++ src/components/GamesList.svelte
@@ -5,6 +5,14 @@
 <main>
   {#each games as game}
     <article>
+      {#if game.image}
+        <picture>
+          <img
+            src={game.image.thumb_url}
+            alt={`Thumbnail for the game ${game.name}`}
+          />
+        </picture>
+      {/if}
       {game.name}
     </article>
   {/each}
